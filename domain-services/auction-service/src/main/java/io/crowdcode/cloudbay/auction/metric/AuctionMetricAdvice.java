befb5acfe5a9f0baa4f6ecbfd888a99d2a28bf88
Ingo Düppe <ingo.dueppe@crowdcode.io>
2021-05-05 17:35:30 +0200

    add metrics

---
+++ domain-services/auction-service/src/main/java/io/crowdcode/cloudbay/auction/metric/AuctionMetricAdvice.java
@@ -0,0 +1,53 @@
+package io.crowdcode.cloudbay.auction.metric;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Ingo Düppe (CROWDCODE)
+ */
+@Aspect
+@ComponentScan
+@RequiredArgsConstructor
+@Profile("with-actuator")
+@Component
+public class AuctionMetricAdvice {
+
+    private final MeterRegistry meterRegistry;
+
+    private Counter auctionCounter;
+
+    private Counter bidCounter;
+
+    @PostConstruct
+    public void init() {
+        auctionCounter = Counter.builder("auctions.auction_total_count")
+                .tag("type", "total")
+                .description("Total number of received auctions")
+                .register(meterRegistry);
+
+        bidCounter = Counter.builder("auctions.bid_total_count")
+                .tag("type", "total")
+                .description("Total number of received bids")
+                .register(meterRegistry);
+    }
+
+    @Before("execution(* *..*.*.placeBidOnProduct(..))")
+    public void countPlaceBidEvent() {
+        bidCounter.increment();
+    }
+
+    @Before("execution(* *..*.*.startAuction(..))")
+    public void countAuctions() {
+        auctionCounter.increment();
+    }
+
+}
---
+++ domain-services/auction-service/src/main/java/io/crowdcode/cloudbay/auction/metric/MetricConfiguration.java
@@ -0,0 +1,16 @@
+package io.crowdcode.cloudbay.auction.metric;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author Ingo Düppe (CROWDCODE)
+ */
+@Slf4j
+@Configuration
+@EnableAspectJAutoProxy
+public class MetricConfiguration {
+
+
+}
