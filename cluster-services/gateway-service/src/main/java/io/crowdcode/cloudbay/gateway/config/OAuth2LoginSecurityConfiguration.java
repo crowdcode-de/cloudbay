c44f5c63463b347eba8cdc9e8593308f2f4b9672
Ingo Düppe <ingo.dueppe@crowdcode.io>
2023-03-22 21:33:56 +0100

    Upgrade to Spring Framework 6

--- cluster-services/gateway-service/src/main/java/io/crowdcode/cloudbay/gateway/config/OAuth2LoginSecurityConfiguration.java
+++ cluster-services/gateway-service/src/main/java/io/crowdcode/cloudbay/gateway/config/OAuth2LoginSecurityConfiguration.java
@@ -2,18 +2,18 @@ package io.crowdcode.cloudbay.gateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * @author Ingo Düppe (CROWDCODE)
  */
 @Configuration
-public class OAuth2LoginSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class OAuth2LoginSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+
+    public SecurityFilterChain filterSecurityChain(HttpSecurity http) throws Exception {
+        return http
             .cors().configurationSource((request) -> {
                 CorsConfiguration corsConfiguration = new CorsConfiguration();
                 corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
@@ -24,12 +24,13 @@ public class OAuth2LoginSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","/index.html","/favicon.png").permitAll()
+                .requestMatchers("/","/index.html","/favicon.png").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
                 .and()
                 .logout()
-        ;
+                .and()
+                .build();
     }
 }
--- cluster-services/gateway-service/src/main/java/io/crowdcode/cloudbay/gateway/config/OAuth2ResourceSecurityConfiguration.java
+++ cluster-services/gateway-service/src/main/java/io/crowdcode/cloudbay/gateway/config/OAuth2ResourceSecurityConfiguration.java
@@ -1,36 +1,39 @@
 package io.crowdcode.cloudbay.gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * @author Ingo Düppe (CROWDCODE)
  */
 @Configuration
-public class OAuth2ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class OAuth2ResourceSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().configurationSource((request) -> {
-            CorsConfiguration corsConfiguration = new CorsConfiguration();
-            corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
-            corsConfiguration.addAllowedOrigin(CorsConfiguration.ALL);
-            corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
-            return corsConfiguration;
-        })
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors().configurationSource(
+                    (request) -> {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+                        corsConfiguration.addAllowedOrigin(CorsConfiguration.ALL);
+                        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+                        return corsConfiguration;
+                })
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index.html", "/favicon.png").permitAll()
+                .requestMatchers("/", "/index.html", "/favicon.png").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer().jwt()
-        ;
+                .and()
+                .and()
+                .build()
+                ;
     }
-
 
 
 }
